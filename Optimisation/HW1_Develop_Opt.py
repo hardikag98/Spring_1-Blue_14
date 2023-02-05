9eedb68709bc4bce077439fb64529ecc9c52102b
janeoya <janeoyaw@outlook.com>
2023-02-05 15:46:15 -0500

    Added new program to perform optimization and create the efficient frontier graph- will update once updated data is available

---
+++ Optimisation/HW1_Develop_Opt.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 14:43:43 2023
+
+@author: Jane
+"""
+
+# %% Import
+import gurobipy as gp
+
+from gurobipy import GRB
+
+import numpy as np
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import math as math
+
+# %% PERFORM OPTIMIZATION
+
+
+# Read in the data
+raw_stock=pd.read_csv('stocks_final.csv')
+
+# Manipulate the data using pivot to get one row per date, the stock names as columns, and the %return values as data points
+
+data_stock= raw_stock.pivot(index='Date', columns="Name", values="Percent Return")
+
+stocks = data_stock.columns
+
+# Get the number of stocks - we know this is 5
+num_stocks=len(stocks)
+
+# Get all of the expected returns - average return for each asset - vector of means
+stock_return = data_stock.mean()
+
+# Get covariance matrix - 5 x 5 matrix with variances down the diagonal (5 stocks)
+cov_mat=data_stock.cov()
+
+# Create an empty model
+m = gp.Model('Bank_Stocks')
+
+# Add a variable for each stock with an lb(lower bound) of zero - defaults to continuous
+vars = pd.Series(m.addVars(stocks,lb=0), index=stocks)  
+
+# update and keep these 5 continuous decision variables
+m.update()
+print(m)
+
+# Calculate risk as variance - use matrix multiplication, multiplying the covariance matrix by the decision var series twice
+portfolio_risk = cov_mat.dot(vars).dot(vars)
+
+# Set objective - minimize the risk
+m.setObjective(portfolio_risk, GRB.MINIMIZE)
+
+# Set constraints
+
+# #1 - sum of all decision variables, all percentages of stocks, needs to equal 1
+m.addConstr(vars.sum() == 1, 'budget')
+
+# #2 - return needs to be more than or equal to 0.005 
+m.addConstr(stock_return.dot(vars) >=0.005,'return')
+
+# update constraints
+m.update()
+print(m)
+
+# lastly, optimize
+m.optimize()
+
+## Optimal objective 1.95140948e+00
+
+print('Minimum Risk Portfolio:\n')
+
+for v in vars:
+    if v.x > 0:
+        print('\t%s\t: %g' % (v.varname, v.x*100)) # prints it in percentage
+#	C0 - ASC	:   8.5258 
+#	C1 - GLNCY: 69.6149
+#	C2 - PDD	:   3.21747e-08 ~ essentially do not invest anything in PDD
+#	C3 - ROST:  19.7577
+#	C4 - TH:    2.10162
+
+
+# print the minimized variance (lowest volatility of the portfolio on a daily basis) when the return is good enough (0.005 here)
+print(m.objval)
+# 1.95
+
+# print std
+print(math.sqrt(m.objval))
+# 1.40
+
+# you would need to compare this to other volitilities to see if it is really good or bad, but it is the best we can do under the constraints
+
+
+# %% Efficient Frontier
+
+# determine a set of portfolios that offer the best returns for a given risk, or the best risk for a given return, over all possible values
+
+# Use same data_stock data frame
+
+# calculate a new stock_volatility object that consists of the std of data_stock (before only calculated mean and cov)
+stock_volatility = data_stock.std()
+
+# print out the average returns for each stock to see the range of the means
+print(stock_return)
+
+# iterate over 0.03 -> 0.621 returns, since this is the range of the means - 100 values
+
+returns = np.linspace( 0.03, 0.621, 100)
+
+# initialize a list of returns (0.03 - 0.621 100 values), the lowest risk at each return, and the optimal proportions for the lowest risks
+ret_list = []
+risks = []
+props = []
+
+# loop through all 100 return values - saving risk and returns to plot
+for ret in returns:
+    m.reset(0)
+    m = gp.Model("Portfolio_Optimization")
+    m.setParam('OutputFlag', 0)
+    vars=pd.Series(m.addVars(stocks,lb=0), index=stocks) 
+    portfolio_risk = cov_mat.dot(vars).dot(vars)
+    m.setObjective(portfolio_risk, GRB.MINIMIZE)
+    m.addConstr(vars.sum() == 1, name = 'budget' )
+    m.addConstr(stock_return.dot(vars) == ret , name = 'return_sim' ) # additional constraint so the return equals the return value specified
+  
+    m.optimize()  
+    
+    # add the std of risk to the risk list
+    risks.append(np.sqrt(m.objval ) )
+    
+    # add the portfolio return to the return list
+    ret_list.append(stock_return.dot(m.x) )
+    
+    # add the proportion matrix to a list
+    props.append(m.x)
+
+# plot
+plt.rcParams.update({'font.size': 15})
+plt.figure(figsize=(600/72,400/72)) # make the right size for the google doc
+plt.plot( risks, returns ) 
+plt.xlabel( 'Risk' )
+plt.ylabel( 'Return' )
+plt.title( 'Efficient Frontier' )
+plt.locator_params(axis='both', nbins=10)
+plt.plot()
+plt.show()
+
+# Look at what the return is at the minimized risk
+min_risk = min(risks)
+index = risks.index(min_risk)
+return_at_min = returns[index]
+
+print(min_risk)
+print(return_at_min)
+# at a risk of 1.39 std (same as 1.95 var, the return is 0.185 or 18.5%)
