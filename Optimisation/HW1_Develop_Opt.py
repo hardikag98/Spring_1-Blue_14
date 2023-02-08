b4347c6d47574b0ee5610076ce9c4cc46511f3e8
janeoya <janeoyaw@outlook.com>
2023-02-08 18:01:44 -0500

    Updated Develop_Opt with new csv file and output the efficient frontier plot

--- Optimisation/HW1_Develop_Opt.py
+++ Optimisation/HW1_Develop_Opt.py
@@ -19,7 +19,6 @@ import matplotlib.pyplot as plt
 
 import math as math
 
-import statistics as statistics
 
 # %% PERFORM OPTIMIZATION
 
@@ -80,20 +79,20 @@ print('Minimum Risk Portfolio:\n')
 for v in vars:
     if v.x > 0:
         print('\t%s\t: %g' % (v.varname, v.x*100)) # prints it in percentage
-#	C0 - ASC	:   8.5258 
-#	C1 - GLNCY: 69.6149
-#	C2 - PDD	:   3.21747e-08 ~ essentially do not invest anything in PDD
-#	C3 - ROST:  19.7577
-#	C4 - TH:    2.10162
+#	C0 - ASC	:   19.9496
+#	C1 - GLNCY: 31.2259
+#	C2 - PDD	:   1.27598e-06 ~ essentially do not invest anything in PDD
+#	C3 - ROST:  40.2282
+#	C4 - TH:    8.59626
 
 
 # print the minimized variance (lowest volatility of the portfolio on a daily basis) when the return is good enough (0.005 here)
 print(m.objval)
-# 1.95
+# 3.72
 
 # print std
 print(math.sqrt(m.objval))
-# 1.40
+# 1.93
 
 # you would need to compare this to other volitilities to see if it is really good or bad, but it is the best we can do under the constraints
 
@@ -110,9 +109,9 @@ stock_volatility = data_stock.std()
 # print out the average returns for each stock to see the range of the means
 print(stock_return)
 
-# iterate over 0.03 -> 0.621 returns, since this is the range of the means - 100 values
+# iterate over 0.03 -> 0.621 returns, since this is the range of the means - 500 values
 
-returns = np.linspace( 0.03, 0.621, 100)
+returns = np.linspace( 0.046, 0.749, 500)
 
 # initialize a list of returns (0.03 - 0.621 100 values), the lowest risk at each return, and the optimal proportions for the lowest risks
 ret_list = []
@@ -145,11 +144,12 @@ for ret in returns:
 plt.rcParams.update({'font.size': 15})
 plt.figure(figsize=(600/72,400/72)) # make the right size for the google doc
 plt.plot( risks, returns ) 
-plt.xlabel( 'Risk' )
+plt.xlabel( 'Risk (standard deviation)' )
 plt.ylabel( 'Return' )
-plt.title( 'Efficient Frontier' )
+plt.title( 'Portfolio Optimization Efficient Frontier' )
 plt.locator_params(axis='both', nbins=10)
 plt.plot()
+plt.savefig("Efficient_Frontier.png", bbox_inches='tight')
 plt.show()
 
 # Look at what the return and proportions are at the minimized risk
@@ -158,9 +158,7 @@ index = risks.index(min_risk)
 return_at_min = returns[index]
 prop_at_min= props[index]
 
-
-# at a risk of 1.39 std (same as 1.95 var, the return is 0.185 or 18.5%)
-# proportions - [0.08535602232123765, 0.6959185629479502, 2.779216229057393e-09, 0.19758900405162302, 0.021136407899972655]
+# at a risk of 1.929 std (same as 3.72 var, the return is 0.212 or 21.2%)
 print("minimum risk (std)=",round(min_risk,3))
 print("return value at the minimum risk",round(return_at_min,3))
 print("Proportion of ASC (Ardmore Shipping Corp) Stock",round(prop_at_min[0],3))
@@ -170,11 +168,9 @@ print("Proportion of RST (Ross Stores) Stock",round(prop_at_min[3],3))
 print("Proportion of TH (Target Hospitality) Stock",round(prop_at_min[4],3))
 
 
-
-
 # Look at what the return and proportions are at a specified risk
 
-target = 2
+target = 2.5
 
 min_difference = float('inf')
 index = None
